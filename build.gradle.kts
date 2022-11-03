8e027d17b727ae995f9f05b9cc11a17b57592c4e
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-11-03 20:08:44 +0300

    ci(github): github actions setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,25 +5,29 @@ val prometheusVersion : String by project
 val koinVersion: String by project
 val koinKtor: String by project
 val mockkVersion: String by project
+val appVersion: String by project
+val detektVersion: String by project
 
 plugins {
     application
     kotlin("jvm") version "1.7.20"
     id("io.ktor.plugin") version "2.1.2"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.7.20"
+    id("io.gitlab.arturbosch.detekt") version "1.21.0"
+}
+
+repositories {
+    mavenCentral()
 }
 
 group = "io.github.sanctumlabs"
-version = "0.0.1"
+version = appVersion
+
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
-}
-
-repositories {
-    mavenCentral()
 }
 
 dependencies {
@@ -48,4 +52,9 @@ dependencies {
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
     testImplementation("io.mockk:mockk:$mockkVersion")
+}
+
+detekt {
+    toolVersion = detektVersion
+    source = files("src/main/kotlin", "src/test/kotlin")
 }
